026b06d6f68dc1a10886a3be8bd44432c2c6d59d
kumar-chetan <112582415+kumar-chetan@users.noreply.github.com>
2022-10-03 21:22:29 +0530

    Add files via upload
    lecture 2

---
+++ lecture2.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+ 
+using namespace std ;
+
+ int main() {
+    
+    int a = 123 ;
+    int s_a=sizeof(a) ;
+    cout<<"Size of int a is "<<s_a<<endl;
+
+    float b= 123.55 ;
+    float s_b=sizeof(b);
+    cout<<"Size of float b is "<<s_b<<endl;
+
+    char c='a';
+    int s_c=sizeof(c);
+    cout<<"Size of char c is "<<s_c<<endl;
+
+    bool d=true ;
+    int s_d=sizeof(d);
+    cout<<"Size of bool d is "<<s_d<<endl;
+
+    double e=23412343.343 ;
+    int s_e=sizeof(e);
+    cout<<"Size of double e is "<<s_e<<endl;
+
+   //  int i =12345678999999976543123;
+    // int y=10999999990;
+    // cout<<y<<endl;
+
+    
+    //  int a='a',b=99000000000;
+  //  cout<<a<<char(b)<<endl; //type Casting explicite homework
+
+   cout<<"OPERATORS:"<<endl;
+   cout<<"Arithmetic (+,-,*,/,%)"<<endl;
+
+ }
