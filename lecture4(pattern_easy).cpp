29dba39df5d158c7f7992764fb44b4dee1e2a034
kumar-chetan <112582415+kumar-chetan@users.noreply.github.com>
2022-10-05 22:17:11 +0530

    Add files via upload
    Lecture 4(pattern_easy)

---
+++ lecture4(pattern_easy).cpp
@@ -0,0 +1,437 @@
+#include<iostream>
+
+using namespace std ;
+
+int main(){
+
+    //Pattern 1 :-
+    // * * * * 
+    // * * * * 
+    // * * * * 
+    // * * * * 
+    // int n ;
+    // cout<<"Enter the value of n :";
+    // cin>>n;
+    // int i=1;
+    // while(i<=n){
+    //     int j=1;
+    //     while(j<=n){
+    //         cout<<"* ";
+    //         j=j+1;
+    //     }
+    //     cout<<endl;
+    //     i = i+1 ;
+    // }
+    //Pattern 2 :- 
+    // 1 1 1 1
+    // 2 2 2 2 
+    // 3 3 3 3 
+    // 4 4 4 4
+
+    // int n ;
+    // cout<<"Enter Value of n :";
+    // cin>>n;
+    // int i=1;
+    // while(i<=n){
+    //     int j=1 ;
+    //     while(j<=n){
+    //         cout<<i<<" ";
+    //         j=j+1;
+    //     }
+    //     cout<<endl;
+    //     i=i+1;
+    // }
+
+
+    //Pattern 3 :-
+    // 1 2 3 4 
+    // 1 2 3 4 
+    // 1 2 3 4 
+    // 1 2 3 4
+
+    // int n ;
+    // cout<<"Enter Value of n :";
+    // cin>>n;
+    // int i=1 ;
+
+    // while(i<=n){
+
+    //     int j=1 ;
+
+    //     while(j<=n){
+    //         cout<<j<<" ";
+    //         j=j+1;
+    //     }
+    //     cout<<endl;
+    //     i=i+1;
+    // }
+    
+    //Home Work 
+    // 4 3 2 1
+    // 4 3 2 1
+    // 4 3 2 1
+    // 4 3 2 1
+
+    // int n ;
+    // cout<<"Enter Value of n :";
+    // cin>>n;
+    // int i=1 ;
+
+    // while(i<=n){
+
+    //     int j=1 ;
+
+    //     while(j<=n){
+    //         cout<<n-j+1<<" ";
+    //         j=j+1;
+    //     }
+    //     cout<<endl;
+    //     i=i+1;
+    // }
+
+    //Pattern 4 :-
+    
+    // 1 2 3
+    // 4 5 6
+    // 7 8 9
+
+    // int n ;
+    // cout<<"Enter a number :";
+    // cin>>n;
+    
+    // int i=1,count=1 ;
+    // while(i<=n){
+    //     int j=1;
+    //     while(j<=n){
+    //         cout<<count<<" ";
+    //         count=count+1;
+    //         j=j+1;
+    //     }
+    //     cout<<endl;
+    //     i=i+1;
+    // }
+
+    //Pattern 5 :-
+    
+    // * 
+    // * *
+    // * * *
+    // * * * * 
+
+    // int n ;
+    // cout<<"Enter a number :";
+    // cin>>n;
+
+    // int i=1 ;
+
+    // while(i<=n){
+    //     int j=1 ;
+
+    //     while(j<=i){
+    //         cout<<"* ";
+    //         j=j+1;
+    //     }
+    //     cout<<endl;
+    //     i=i+1;
+
+    // }
+
+    //Pattern 6 :-
+    // 1
+    // 2 2
+    // 3 3 3
+    // 4 4 4 4
+
+    // int n ;
+    // cout<<"Enter a number :";
+    // cin>>n;
+
+    // int i=1 ;
+
+    // while(i<=n){
+    //     int j=1 ;
+
+    //     while(j<=i){
+    //         cout<<i<<" ";
+    //         j=j+1;
+    //     }
+    //     cout<<endl;
+    //     i=i+1;
+
+    // }
+
+    //HOME WORK
+    // 1
+    // 2 3
+    // 4 5 6
+    // 7 8 9 10
+    // int n ;
+    // cout<<"Enter a Number :";
+    // cin>>n;
+
+    // int i=1,c=1 ;
+    
+    // while(i<=n){
+    //     int j=1;
+    //     while(j<=i){
+    //         cout<<c<<" ";
+    //         c=c+1;
+    //         j=j+1;
+    //     }
+    //     cout<<endl;
+    //     i=i+1;
+    // }
+
+    //Pattern 7 :-
+    // 1 
+    // 2 3 
+    // 3 4 5 
+    // 4 5 6 7 
+
+    // int n ;
+    // cout<<"Enter a Number :";
+    // cin>>n;
+
+    // int i=1;
+    
+    // while(i<=n){
+    //     int j=1;
+    //     int v=i;
+    //     while(j<=i){
+
+    //         cout<<v<<" ";
+    //         v=v+1;
+    //         j=j+1;
+
+    //     }
+    //     cout<<endl;
+    //     i=i+1;
+    // }
+
+    //Method 2 
+    // int n ;
+    // cout<<"Enter a Number :";
+    // cin>>n;
+
+    // int i=1;
+    
+    // while(i<=n){
+    //     int j=1;
+
+    //     while(j<=i){
+
+    //         cout<<(i+j)-1<<" ";
+            
+    //         j=j+1;
+
+    //     }
+    //     cout<<endl;
+    //     i=i+1;
+    // }   
+
+
+    //Pattern 8 :-
+    // 1 
+    // 2 1 
+    // 3 2 1 
+    // 4 3 2 1
+
+    // int n ;
+    // cout<<"Enter a Number :";
+    // cin>>n;
+
+    // int i = 1 ;
+
+    // while(i<=n){
+    //     int j=1 ;
+    //     while(j<=i){
+    //         cout<<(i-j)+1<<" ";
+    //         j=j+1;
+
+    //     }
+    //     cout<<endl;
+    //     i=i+1;
+    // }
+
+    //Pattern 9 :-
+    // A A A A
+    // B B B B
+    // C C C C 
+    // D D D D 
+
+    // int n ;
+    // cout<<"Enter a Number :";
+    // cin>>n;
+    
+    // int i=1;
+    // while(i<=n){
+    //     int j=1;
+    //     while(j<=n){
+    //         cout<<char('A'+i-1)<<" ";
+    //         j=j+1;
+
+    //     }
+    //     cout<<endl;
+    //     i=i+1;
+    // }
+
+    // Pattern 10 :-
+    // A B C D
+    // A B C D
+    // A B C D
+    // int n ;
+    // cout<<"Enter a Number :";
+    // cin>>n;
+    
+    // int i=1;
+    // while(i<=n){
+    //     int j=1;
+    //     while(j<=n){
+    //         cout<<char('A'+j-1)<<" ";
+    //         j=j+1;
+
+    //     }
+    //     cout<<endl;
+    //     i=i+1;
+    // }
+
+    //HOME WORK
+    // A B C 
+    // D E F
+    // G H I
+
+    // int n ;
+    // cout<<"Enter a Number :";
+    // cin>>n;
+
+    // int i = 1 ,k=0;
+
+    // while(i<=n){
+    //     int j=1 ;
+    //     while(j<=n){
+    //         cout<<char('A'+k)<<" ";
+    //         k=k+1;
+    //         j=j+1;
+    //     }
+    //     cout<<endl;
+    //     i=i+1;
+    // }
+
+    //Pattern 11 :-
+
+    // int n ;
+    // cout<<"Enter a Number :";
+    // cin>>n;
+
+    // int i = 1 ;
+
+    // while(i<=n){
+    //     int j=1;
+    //     while(j<=n){
+    //         cout<<char('A'+(i+j-1)-1)<<" ";
+    //         j=j+1;
+    //     }
+    //     cout<<endl;
+    //     i=i+1;
+    // }
+
+    //Pattern 12 :-
+
+    // A
+    // B B 
+    // C C C 
+
+    // int n ;
+    // cout<<"Enter a Number :";
+    // cin>>n;
+
+    // int i =1;
+    
+    // while(i<=n){
+    //     int j=1;
+
+    //     while(j<=i){
+    //         cout<<char('A'+i -1)<<" ";
+    //         j=j+1;
+    //     }
+    //     cout<<endl;
+    //     i=i+1;
+    // }
+
+    //Pattern 13 :-
+    // A 
+    // B C
+    // D E F 
+    // G H I J
+
+    // int n ;
+    // cout<<"Enter a Number :";
+    // cin>>n;
+
+    // int i = 1,k=0 ;
+
+    // while(i<=n){
+    //     int j = 1 ;
+    //     while(j<=i){
+    //         cout<<char('A'+k)<<" ";
+    //         j=j+1;
+    //         k=k+1;
+    //     }
+    //     cout<<endl;
+    //     i=i+1;
+    // }
+
+    // Pattern 14 :-
+    // A       (1,1)=2
+    // B C     (2,1) (2,2)=3,4
+    // C D E   (3,1) (3,2) (3,3)=4,5,6
+    // D E F G (4,1) (4,2) (4,3) (4,4)=5,6,7,8
+    
+    // int n ;
+    // cout<<"Enter a Number :";
+    // cin>>n;
+
+    // int i=1;
+
+    // while(i<=n){
+    //     int j=1;
+
+    //     while(j<=i){
+
+    //         cout<<char('A'+i+j-2)<<" ";
+    //         j=j+1;
+
+    //     }
+    //     cout<<endl;
+    //     i=i+1;
+    // }
+
+    //Pattern 15 :-
+
+    // D 
+    // C D
+    // B C D
+    // A B C D 
+
+    int n ;
+    cout<<"Enter a Number :";
+    cin>>n;
+
+    int i = 1 ;
+
+    while(i<=n){
+
+        int j=1 ;
+        char ch = 'A'+n-i;
+        while(j<=i){
+
+            
+            cout<<ch<<" ";
+            ch=ch+1;
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+
+}
